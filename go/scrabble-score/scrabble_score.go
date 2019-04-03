6db918c1969fed0b570573824f2cc99aa9140517
Andrea Gómez <crypto.andrea@protonmail.ch>
2019-04-03 15:26:54 -0600

    Go: Scrabble Score

---
+++ go/scrabble-score/scrabble_score.go
@@ -0,0 +1,29 @@
+package scrabble
+
+import (
+	"unicode"
+)
+
+// Score computes the scrabble score for a given word.
+func Score(word string) (score int) {
+	for _, char := range word {
+		switch unicode.ToLower(char) {
+		case 'a', 'e', 'i', 'o', 'u', 'l', 'n', 'r', 's', 't':
+			score += 1
+		case 'd', 'g':
+			score += 2
+		case 'b', 'c', 'm', 'p':
+			score += 3
+		case 'f', 'h', 'v', 'w', 'y':
+			score += 4
+		case 'k':
+			score += 5
+		case 'j', 'x':
+			score += 8
+		case 'q', 'z':
+			score += 10
+		}
+	}
+
+	return score
+}
