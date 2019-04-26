0be9a69f00a904f06808842de800409ce739d015
Andrea Gómez <crypto.andrea@protonmail.ch>
2019-04-26 17:10:06 -0500

    TypeScript: Hello, World

---
+++ typescript/hello-world/build/hello-world.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class HelloWorld {
+    static hello(name = 'World') {
+        return `Hello, ${name}!`;
+    }
+}
+exports.default = HelloWorld;
+//# sourceMappingURL=hello-world.js.map
